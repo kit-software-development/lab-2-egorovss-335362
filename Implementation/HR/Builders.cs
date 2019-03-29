6291eb53c9c21a0355cc69480a2defa83b0a7ab6
Sergey Egorov <astron280@mail.ru>
2019-03-29 10:39:44 +0300

    done

--- Implementation/HR/Builders.cs
+++ Implementation/HR/Builders.cs
@@ -1,4 +1,6 @@
 using System;
+using Practice.Common;
+using Practice.Organization;
 
 namespace Practice.HR
 {
@@ -14,12 +16,41 @@ namespace Practice.HR
         /// <returns>
         ///     Экземпляр типа IClientBuilder.
         /// </returns>
+
+        internal class clientbuilder : IClientBuilder
+        {
+            private IClient client = new Client();
+
+            public IClient Build()
+            {
+                return client;
+            }
+
+            public IClientBuilder Name(IName name)
+            {
+                client.Name = new Name(name);
+                return this;
+            }
+
+            public IClientBuilder Discount(float discount)
+            {
+                client.Discount = discount;
+                return this;
+            }
+
+            public IClientBuilder Name(string name, string surname, string patronymic)
+            {
+                client.Name = new Name(name, surname, patronymic);
+                return this;
+             }
+        }
+
         public static IClientBuilder ClientBuilder()
         {
             /*
              * TODO #6: Реализовать фабричный метод ClientBuilder класса Builders
              */
-            throw new NotImplementedException();
+            return new clientbuilder();
         }
 
         /// <summary>
@@ -33,7 +64,47 @@ namespace Practice.HR
             /*
              * TODO #7: Реализовать фабричный метод EmployeeBuilder класса Builders
              */
-            throw new NotImplementedException();
+            return new employeebuilder();
+        }
+
+        internal class employeebuilder : IEmployeeBuilder
+        {
+            private Employee emp = new Employee();
+
+           public IEmployee Build()
+           {
+                return emp;
+           }
+
+            public IEmployeeBuilder Department(IDepartment dep)
+            {
+                emp.Department = dep;
+                return this;
+            }
+
+
+            public IEmployeeBuilder Name(IName name)
+            {
+                emp.Name = new Name(name);
+                return this;
+            }
+
+            public IEmployeeBuilder Department(string dep)
+            {
+
+                emp.Department = new Department(dep);
+
+                return this;
+            }
+
+
+            public IEmployeeBuilder Name(string name, string surname, string patronymic)
+
+            {
+                emp.Name = new Name(name, surname, patronymic);
+                return this;
+            }
+
         }
 
     }
--- Implementation/HR/Client.cs
+++ Implementation/HR/Client.cs
@@ -1,12 +1,25 @@
-﻿namespace Practice.HR
+﻿using System;
+using Practice.Common;
+using Practice.HR.Events;
+
+namespace Practice.HR
 {
     /// <summary>
     ///     Скрытая реализация представления о клиенте.
     /// </summary>
-    internal class Client : AbstractPerson
+    internal class Client : AbstractPerson, IClient
     {
-        /*
-         * TODO #4: Реализуйте интерфейс IClient для класса Client
-         */
-    }
+        private float v_discount;
+        public float Discount
+        {
+                get { return v_discount; }
+                set {  DiscountChange?.Invoke(this, new ValueChangeEventArgs<float>(v_discount));
+                       v_discount = value; }
+        }
+
+    public event EventHandler<ValueChangeEventArgs<float>> DiscountChange;
+    /*
+     * TODO #4: Реализуйте интерфейс IClient для класса Client
+     */
+}
 }
